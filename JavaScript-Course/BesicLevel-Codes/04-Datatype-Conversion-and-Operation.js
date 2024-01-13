17a6335fc4bc603094284b66c65d751b47b1c418
KNIGHTCORE47 <145120465+KNIGHTCORE47@users.noreply.github.com>
2024-01-13 13:36:53 +0000

    04-Conversion

---
+++ JavaScript-Course/BesicLevel-Codes/04-Datatype-Conversion-and-Operation.js
@@ -0,0 +1,37 @@
+let age = "30";
+let age2 = "1f4";
+
+let isLoggedIn = 1; // it will give true and if value is 0 give false
+
+let someNum = 44;
+
+console.log(typeof age);
+console.log(typeof (age));
+
+// Conversion
+
+let convertValueAge = Number(age);
+let convertValueAge2 = Number(age2);    // it will give NaN value
+
+let convertValueisLoggedIn = Boolean(isLoggedIn);
+
+let convertValuesomeNum = String(someNum);
+
+console.log(typeof convertValueAge);
+console.log(typeof convertValueAge2);
+console.log(convertValueAge2);
+
+console.log(typeof convertValueisLoggedIn);
+console.log(convertValueisLoggedIn);
+
+console.log(typeof convertValuesomeNum);
+console.log(convertValuesomeNum);
+
+//"33" => 33
+// "33ddd" => NaN (Not a Number)
+// 1 => true; 0 => false;
+// "" => false ; "abc" => true;
+
+
+//Operations
+
