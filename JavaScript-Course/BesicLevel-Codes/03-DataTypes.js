c560fd4e098c8632a683ab23cc12b9d84e22f3e0
KNIGHTCORE47 <145120465+KNIGHTCORE47@users.noreply.github.com>
2024-01-13 12:56:21 +0000

    03 Push

---
+++ JavaScript-Course/BesicLevel-Codes/03-DataTypes.js
@@ -0,0 +1,29 @@
+"use strict";       //treat all JS code as newer version ES6
+
+// alert(3+3);
+
+let name = "abc"    //string datatype
+
+let age = 82;        // Number datatype 
+
+let isKid = false;  // boolean datatype 
+
+let tempStatus = null;
+
+/* These all are primitive datatypes - 
+
+// number => 2 to the power 53 **approx
+// bigint 
+// string = ""
+// boolean = false/true
+// null => standalone value
+// undefined => undefined
+// symbol => use for uniqueness
+*/
+
+//object
+
+console.log(typeof name);
+console.log(typeof tempStatus);
+console.log(typeof null);
+console.log(typeof undefined);
