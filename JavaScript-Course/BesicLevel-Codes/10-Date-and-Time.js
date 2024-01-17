1e24155cf69db2f4927f10bc5ac2655051f64544
KNIGHTCORE47 <145120465+KNIGHTCORE47@users.noreply.github.com>
2024-01-17 15:35:56 +0000

    10 Time Push

--- JavaScript-Course/BesicLevel-Codes/10-Date-and-Time.js
+++ JavaScript-Course/BesicLevel-Codes/10-Date-and-Time.js
@@ -2,6 +2,8 @@
 
 let theDate = new Date();
 
+console.log(theDate);
+
 console.log("toDateString()");
 console.log(theDate.toDateString());
 
@@ -26,3 +28,34 @@ console.log(myDeclaredDate.toDateString());
 let myDeclaredDateAndTime = new Date(2024, 0, 16, 10, 32);
 
 console.log(myDeclaredDateAndTime.toLocaleString());
+
+let anotherMyDate = new Date("01-17-2024")      //MM-DD-YY
+console.log(anotherMyDate.toLocaleString()); 
+
+let myTimeStamp = Date.now();       //return date in milisecond from 1st jan 1970
+
+console.log(myTimeStamp);
+console.log(anotherMyDate.getTime());
+
+console.log(Math.floor(Date.now()*1000));      //return date in seconds from 1st jan 1970, here Math.floor() used to round off the decimal value
+
+let newDate = new Date();
+
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+/* 
+.toLocaleDateString() it is very unique Converts a date to a string by using the current or specified locale.
+
+@param locales — A locale string, array of locale strings, Intl.Locale object, or array of Intl.Locale objects that contain one or more language or locale tags. If you include more than one locale string, list them in descending order of priority so that the first entry is the preferred locale. If you omit this parameter, the default locale of the JavaScript runtime is used.
+
+@param options — An object that contains one or more properties that specify comparison options.
+*/
+
+let changeDate = newDate.toLocaleDateString("default", {
+    month: "long",
+    hour: "numeric",
+})
+
+console.log(changeDate);
