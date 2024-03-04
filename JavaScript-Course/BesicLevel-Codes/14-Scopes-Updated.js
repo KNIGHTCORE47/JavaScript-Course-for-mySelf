c96f68e2f32b4caffcc39b9413ccb543f244919f
KNIGHTCORE47 <145120465+KNIGHTCORE47@users.noreply.github.com>
2024-03-04 19:02:57 +0000

    concept of hoisting and scopes

--- JavaScript-Course/BesicLevel-Codes/14-Scopes-Updated.js
+++ JavaScript-Course/BesicLevel-Codes/14-Scopes-Updated.js
@@ -38,10 +38,58 @@ function nested() {
         const userPassword = "Knight#40Pass";
         console.log(userName);
     }
-    console.log(userPassword);
+    // console.log(userPassword);
     nestedOne()
 }
 nested()
 
 
 //Closures
+
+function one() {
+    const winScoreBy = "1";
+    function two() {
+        const wonTeam = "Brazil";
+
+        console.log(`${wonTeam} has secured the win with ${winScoreBy} goal`);
+    }
+
+    two();
+}
+
+one();
+
+//it is an example clouser that is the func two() {} which is the child nested inside of func one() {} parent can use it's variables inside of it.
+
+// it also can create with if else condition
+
+if (true) {
+    const oldLoginPass = "16#02#1998theBirth";
+    if (!oldLoginPass) {
+        const newLoginPass = "16#02#1998theBirt";
+        console.log(`Sorry your oldLoginPass - ${oldLoginPass} and newLoginPass - ${newLoginPass} can not be the same `);
+    }else{
+        console.log("Access Denied!");
+        // console.log(`Sorry your oldLoginPass - ${oldLoginPass} and newLoginPass - ${newLoginPass} can not be the same `);
+
+    }
+}
+
+
+
+//concept of Hoisting
+
+calculateNum(5, 8);
+
+function calculateNum (num1, num2) {
+    return console.log(num1 + num2);
+}
+
+
+// calculateNum02(14, -16);
+
+const calculateNum02 = function (num1, num2) {  //it is called a function expression cause the actual function is hold inside of a constant, it is why there is no chance of getting hoisting here means expression of the function is declired before that actual function even initialized causing errors.
+    return console.log(num1 + num2);
+}
+
+calculateNum02(14, -16);
